e7dd3a52e71b4a9ceab98756c287c7106388e07c
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2024-02-07 14:28:53 +0000

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+import noteReducer from "./reducers/noteReducer.js";
+
+const store = createStore(noteReducer);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
