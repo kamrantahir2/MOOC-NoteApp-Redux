9aecd126a7dd556ad63e549740b4819171cadc50
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2024-02-11 21:08:27 +0000

    Create radio buttons for filtering notes

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,43 @@ import NewNote from "./components/NewNote";
 import Notes from "./components/Notes";
 
 function App() {
-  const dispatch = useDispatch();
-  const notes = useSelector((state) => state);
+  const filterSelected = (value) => {
+    console.log(value);
+  };
 
   return (
     <div>
       <NewNote />
+
+      <div>
+        <div>
+          <input
+            type="radio"
+            name="filter"
+            onChange={() => filterSelected("ALL")}
+          />{" "}
+          all
+        </div>
+
+        <div>
+          <input
+            type="radio"
+            name="filter"
+            onChange={() => filterSelected("IMPORTANT")}
+          />
+          important
+        </div>
+
+        <div>
+          <input
+            type="radio"
+            name="filter"
+            onChange={() => filterSelected("NOTIMPORTANT")}
+          />
+          not important{" "}
+        </div>
+      </div>
+
       <Notes />
     </div>
   );
