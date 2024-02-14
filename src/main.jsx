c6e78a2c005b115cba1a64e19f36009a2f8f27f6
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2024-02-14 17:11:39 +0000

    Clean up main.jsx file by moving store related code to new file store.js

--- src/main.jsx
+++ src/main.jsx
@@ -3,20 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
-import { createNote } from "./reducers/noteReducer.js";
-import { configureStore } from "@reduxjs/toolkit";
-import noteService from "./services/notes.js";
-import noteReducer, { setNotes, appendNote } from "./reducers/noteReducer.js";
-import filterReducer from "./reducers/filterReducer.js";
-
-const store = configureStore({
-  reducer: {
-    notes: noteReducer,
-    filter: filterReducer,
-  },
-});
-
-store.subscribe(() => console.log(store.getState()));
+import store from "./store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
---
+++ src/store.js
@@ -0,0 +1,14 @@
+import { configureStore } from "@reduxjs/toolkit";
+import noteReducer from "./reducers/noteReducer";
+import filterReducer from "./reducers/filterReducer";
+
+const store = configureStore({
+  reducer: {
+    notes: noteReducer,
+    filter: filterReducer,
+  },
+});
+
+store.subscribe(() => console.log(store.getState()));
+
+export default store;
